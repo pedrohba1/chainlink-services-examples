aeeabdde04bc241a4c67324fcd4dd3f34632daeb
pedro bufulin <pedro.almeida@ufu.br>
2022-12-22 16:23:10 -0300

    feat: VRF consumer

--- hardhat/test/testnet/price_feed/_1.ts
+++ hardhat/test/testnet/price_feed/_1.ts
@@ -2,17 +2,8 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { PriceConsumerV3 } from "../../../types/contracts/PriceConsumer.sol";
 
-/**
- * Test against multiWord contract from chainlink
- * It restricts Consumer addresses based on the .toml configuration only.
- *
- * The Oracle addess specified in the oracle also needs to be set up correctly.
- *
- * Rinkeby LINK Address: 0x01BE23585060835E02B77ef475b0Cc51aA1e0709
- *
- * These tests are separated in two test files, since testing given the chainlink node requires
- * setting the oracle address and there isn't an easy way to do it in typescript tests.
- */
+
+
 
 const NODE_ADDRESS = String(process.env.NODE_ADDRESS);
 const LINK_TOKEN_ADDRESS = "0x01BE23585060835E02B77ef475b0Cc51aA1e0709";
